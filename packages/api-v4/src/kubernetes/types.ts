b3118c94e0a95433c0f2ef4efc93fcf790c80b95
mduda <mduda@akamai.com>
2025-08-25 13:09:28 +0200

    upcoming: [DPS-34192] CR changes 1

--- packages/api-v4/src/kubernetes/types.ts
+++ packages/api-v4/src/kubernetes/types.ts
@@ -28,10 +28,6 @@ export interface KubernetesCluster {
   id: number;
   k8s_version: string;
   label: string;
-  /**
-   * Upcoming Feature, Beta - Datastream logs, name of the property to be confirmed
-   */
-  logGeneration?: boolean;
   region: string;
   /**
    * Upcoming Feature Notice - LKE-E:** this property may not be available to all customers
@@ -183,6 +179,10 @@ export interface ControlPlaneACLOptions {
 
 export interface ControlPlaneOptions {
   acl?: ControlPlaneACLOptions;
+  /**
+   * Upcoming Feature, Beta - Datastream logs
+   */
+  audit_logs_enabled?: boolean;
   high_availability?: boolean;
 }
 
